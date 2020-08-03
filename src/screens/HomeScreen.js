865e307275c86175c938cee2ebaeb71040bdc147
Steve Lanier <stevekentlanier@gmail.com>
2020-08-03 12:18:53 -0700

    initalized TouchableOpacity primitive component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = () => {
   return (
@@ -10,6 +10,9 @@ const HomeScreen = () => {
         onPress={() => console.log('Button Pressed!')} 
         title="Go to Components Demo" 
       />
+      <TouchableOpacity>
+        
+      </TouchableOpacity>
     </View>
   );
 };
