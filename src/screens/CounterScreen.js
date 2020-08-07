2738ef9a2cfc2e492af8babc9ffeffb9c9242d97
Steve Lanier <stevekentlanier@gmail.com>
2020-08-06 18:19:10 -0700

    added CounterScreen

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const CounterScreen = () => {
+    return (
+        <View>
+            <Text>
+
+            </Text>
+            <Button>
+
+            </Button>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default CounterScreen;
