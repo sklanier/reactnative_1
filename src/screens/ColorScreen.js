ab921314e6977eea9b1b4a1441f297dbc1e3f66c
Steve Lanier <stevekentlanier@gmail.com>
2020-08-08 12:13:02 -0700

    added randomColor functionality to new styled View in ColorScreen

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -7,8 +7,26 @@ const ColorScreen = () => {
             <Button 
                 title="Add a Color!"
             />
+            <View 
+                style={
+                    {
+                    height: 100, 
+                    width: 100, 
+                    backgroundColor: randomColor()
+                    }
+                }
+            >
+            </View>
         </View>
     );
+};
+
+const randomColor = () => {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+
+    return `rgb(${red}, ${green}, ${blue})`;
 };
 
 const styles = StyleSheet.create({});
