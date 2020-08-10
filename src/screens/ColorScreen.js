45f65418046b8f7aa3146df1e6a2f5598b437ba2
Steve Lanier <stevekentlanier@gmail.com>
2020-08-10 12:15:11 -0700

    Monday initial

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,39 +1,34 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Text, Button, FlatList } from 'react-native';
 
 const ColorScreen = () => {
-    let [colors, setColors] = useState([]);
+	const [colors, setColors] = useState([]);
 
-    return (
-        <View>
-            <Button 
-                title="Add a Color!"
-                onPress={() => {
-                    setColors([...colors, randomColor()]);
-                }}
-            />
-            <View 
-                style={
-                    {
-                    height: 100, 
-                    width: 100, 
-                    backgroundColor: randomColor()
-                    }
-                }
-            >
-            </View>
-        </View>
-    );
-};
+	return (
+		<View>
+			<Button
+				title="Add a Color!"
+				onPress={() => {
+					setColors([...colors, randomColor()]);
+				}}
+			/>
+			
+			<FlatList 
+				data={colors}
+				renderItem={({ item }) => {
+					return<View
+									style={{ height: 100, width: 100, backgroundColor: randomColor() }}
+								/>
+				}}
+			/>
+		</View>
+	)
+}
 
-const randomColor = () => {
-    let red = Math.floor(Math.random() * 256);
-    let green = Math.floor(Math.random() * 256);
-    let blue = Math.floor(Math.random() * 256);
+let randomColor = () => {
+	let red = Math.floor(Math.random() * 256);
+	let green = Math.floor(Math.random() * 256);
+	let blue = Math.floor(Math.random() * 256);
 
-    return `rgb(${red}, ${green}, ${blue})`;
-};
-
-const styles = StyleSheet.create({});
-
-export default ColorScreen;
+	return `rgb(${red}, ${green}, ${blue})`;
+}
