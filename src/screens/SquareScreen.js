5634c54ccb60a6988c8ea4b0613bb2be5e821dd1
Steve Lanier <stevekentlanier@gmail.com>
2020-08-12 10:53:02 -0700

    passed callbacks to each color

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -6,11 +6,23 @@ const SquareScreen = () => {
     const [red, setRed] = useState(0);
     const [green, setGreen] = useState(0);
     const [blue, setBlue] = useState(0);
-    
+
     return <View>
-        <ColorCounter color="Red"/>
-        <ColorCounter color="Blue"/>
-        <ColorCounter color="Green"/>
+        <ColorCounter
+            onIncrease={() => setRed(red + 1)}
+            onDecrease={() => setRed(red - 1)}
+            color="Red"
+        />
+        <ColorCounter
+            onIncrease={() => setGreen(green + 1)}
+            onDecrease={() => setRed(green - 1)}
+            color="Green"
+        />
+        <ColorCounter
+            onIncrease={() => setBlue(blue + 1)}
+            onDecrease={() => setBlue(blue - 1)}
+            color="Blue"
+        />
     </View>
 };
 
